c4b3c5b808fa06a388f7f2cc2ae28253ca5c850c
Muhammad Farid <faridislamudin340@gmail.com>
2026-04-22 17:03:45 +0700

    feat(Global API): memahami general

--- src/Application.vue
+++ src/Application.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+import { version } from 'vue'
+import { ref, nextTick } from 'vue'
+import { defineAsyncComponent } from 'vue'
+
+// Komponen ini HANYA akan di-download saat dibutuhkan
+const Components = defineAsyncComponent(() =>
+  import('./components/WelcomeItem.vue')
+)
+
+const judul = ref('Hello APPLICATION')
+const judulRef = ref(null)
+
+const gantiJudul = async () => {
+  judul.value = 'HELLO WORLD!'
+  await nextTick()
+} // nextTick digunakan untuk memastikan bahwa DOM sudah diperbarui sebelum kita mencoba mengaksesnya
+
+console.log(`Vue version: ${version}`)
+</script>
+
 <template>
-<h1>
-    Hello Application
-</h1>
+  <div>
+    <h2 ref="judulRef">{{ judul }}</h2>
+    <button @click="gantiJudul">Ganti Judul</button>
+  </div>
 </template>
---
+++ src/TestComponent.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      count: 0,
+      username: 'Budi'
+    }
+  },
+  methods: {
+    tambah() {
+      // SAAT KAMU KETIK "this.", VS Code akan memunculkan:
+      // count, username, tambah, dll.
+      this.count++
+    }
+  }
+})
+</script>
